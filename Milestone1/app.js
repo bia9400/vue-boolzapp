0ed88c6380ba479c030bb292703d2cade4b96b8f
bia9400 <ferro.biagio@gmail.com>
2022-06-14 14:59:49 +0200

    almost done

--- Milestone1/app.js
+++ Milestone1/app.js
@@ -160,6 +160,50 @@ const contatti = [
         },
       ],
     },
+    {
+        name: "Davide",
+        avatar: "../img/avatar_8.jpg",
+        visible: false,
+        messages: [
+          {
+            date: "10/01/2020 15:30:55",
+            message: "Ciao, andiamo a mangiare la pizza stasera?",
+            status: "received",
+          },
+          {
+            date: "10/01/2020 15:50:00",
+            message: "No, l'ho già mangiata ieri, ordiniamo sushi!",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 15:51:00",
+            message: "OK!!",
+            status: "received",
+          },
+        ],
+      },
+      {
+        name: "Davide",
+        avatar: "../img/avatar_8.jpg",
+        visible: false,
+        messages: [
+          {
+            date: "10/01/2020 15:30:55",
+            message: "Ciao, andiamo a mangiare la pizza stasera?",
+            status: "received",
+          },
+          {
+            date: "10/01/2020 15:50:00",
+            message: "No, l'ho già mangiata ieri, ordiniamo sushi!",
+            status: "sent",
+          },
+          {
+            date: "10/01/2020 15:51:00",
+            message: "OK!!",
+            status: "received",
+          },
+        ],
+      },
   ];
   
 
